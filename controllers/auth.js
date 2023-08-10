71187c61425c6f637d97312b667c9518f46d5230
Vanessa Balam <sbaltec92@gmail.com>
2023-08-10 12:18:06 -0500

    Fin sección 11 - version 4.0.0

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,7 +6,7 @@ const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/generar-jwt');
 // "correo": "test@hotmail.com",
 // "password": "testpass",
-
+const { googleVerify } = require('../helpers/google-verify');
 const login = async(req, res = response) => {
 
     const { correo, password } = req.body;
@@ -45,6 +45,7 @@ const login = async(req, res = response) => {
         })
 
     } catch (error) {
+
         console.log(error)
         res.status(500).json({
             msg: 'Hable con el administrador'
@@ -54,7 +55,65 @@ const login = async(req, res = response) => {
 }
 
 
+const googleSignIn = async(req, res = response) => {
+
+    const { id_token } = req.body;
+    
+    try {
+        const { correo, nombre, img } = await googleVerify( id_token );
+
+        let usuario = await Usuario.findOne({ correo });
+
+        if ( !usuario ) {
+            // Tengo que crearlo
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                rol: "USER_ROLE",
+                google: true
+            };
+       
+            usuario = new Usuario( data );
+            await usuario.save();
+        }
+ 
+        // // Si el usuario en DB 
+        if ( !usuario.estado ) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+        // // Generar el JWT
+        const token = await generarJWT( usuario.id );
+        
+        res.json({
+            usuario,
+            token
+        });
+
+
+        // res.json({
+        //     msg: 'Todo bien!',
+        //     id_token
+        // });
+        
+    } catch (error) {
+
+        res.status(400).json({
+            msg: 'Token de Google no es válido'
+        })
+
+    }
+
+
+
+}
+
 
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
